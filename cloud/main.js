b32d4af933813cee42b8b78a70388423646f473b
Timothy Brandt <timothy.brandt@gmail.com>
2015-02-22 11:22:28 -0800

    add appSid, define test TwiML endpoints

--- cloud/main.js
+++ cloud/main.js
@@ -10,11 +10,36 @@ Parse.Cloud.define("token", function(request, response) {
   var twilio = require('twilio');
   var accountSID = 'AC17aa64762866918328ad2bdd4382f594';
   var authToken = 'f91d9e5cdb3f08e2d17e410a4601841b';
+  var appSid = 'AP91929ec65ae9377af2c47cd245659c8a';
   var capability = new twilio.Capability(accountSID, authToken);
 
   //Create a capability token using the TwiML app with sid "AP123"
+  capability.allowClientOutgoing(appSid);
   capability.allowClientOutgoing('tim');
   capability.allowClientIncoming('joe');
+  capability.allowClientIncoming('tim');
   var token = capability.generate();
   response.success(token);
+});
+
+// default endpoint
+Parse.Cloud.define("", function(request, response) {
+  var twilio = require('twilio');
+  var resp = new twilio.TwimlResponse();
+  resp.say('Welcome to Twilio!');
+
+  // Render the TwiML XML document
+  response.type('text/xml');
+  response.send(resp.toString());
+});
+
+// say hello
+Parse.Cloud.define("hello", function(request, response) {
+  var twilio = require('twilio');
+  var resp = new twilio.TwimlResponse();
+  resp.say('Hello!');
+
+  // Render the TwiML XML document
+  response.type('text/xml');
+  response.send(resp.toString());
 });
