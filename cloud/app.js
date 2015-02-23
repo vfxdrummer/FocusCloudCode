d8e8bc6f9e6cc90aec5d063569bee24b076d9c37
Timothy Brandt <timothy.brandt@gmail.com>
2015-02-23 00:45:00 -0800

    add express.js webapp to make TwiML requests w/ contentType XML

---
+++ cloud/app.js
@@ -0,0 +1,62 @@
+
+// These two lines are required to initialize Express in Cloud Code.
+var express = require('express');
+var app = express();
+
+// Global app configuration section
+app.set('views', 'cloud/views');  // Specify the folder to find templates
+app.set('view engine', 'ejs');    // Set the template engine
+app.use(express.bodyParser());    // Middleware for reading request body
+
+// This is an example of hooking up a request handler with a specific request
+// path and HTTP verb using the Express routing API.
+app.get('/hello', function(req, res) {
+  res.render('hello', { message: 'Congrats, you just set up your app!' });
+});
+
+// Create a route that will respond to am HTTP GET request with some
+// simple TwiML instructions
+app.get('/testing', function(req, res) {
+  	var twilio = require('twilio');
+    // Create a TwiML response generator object
+    var twiml = new twilio.TwimlResponse();
+ 
+    // add some instructions
+    twiml.say('Hello there! Isn\'t Parse cool?', {
+        voice:'woman'
+    });
+ 
+    // Render the TwiML XML document
+    res.type('text/xml');
+    res.send(twiml.toString());
+});
+
+app.post('/testing', function(req, res) {
+  	var twilio = require('twilio');
+    // Create a TwiML response generator object
+    var twiml = new twilio.TwimlResponse();
+ 
+    // add some instructions
+    twiml.say('Hello there! Isn\'t Parse cool?', {
+        voice:'woman'
+    });
+ 
+    // Render the TwiML XML document
+    res.type('text/xml');
+    res.send(twiml.toString());
+});
+
+// // Example reading from the request query string of an HTTP get request.
+// app.get('/test', function(req, res) {
+//   // GET http://example.parseapp.com/test?message=hello
+//   res.send(req.query.message);
+// });
+
+// // Example reading from the request body of an HTTP post request.
+app.post('/test', function(req, res) {
+  // POST http://example.parseapp.com/test (with request body "message=hello")
+  res.send(req.body.message);
+});
+
+// Attach the Express app to Cloud Code.
+app.listen();
--- cloud/main.js
+++ cloud/main.js
@@ -1,8 +1,4 @@
-var express = require('express');
-var app = express();
-
-// Global app configuration section.
-app.use(express.bodyParser());
+require('cloud/app.js');
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
@@ -10,19 +6,19 @@ Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
 
-// get twilio token
+// // get twilio token
 Parse.Cloud.define("token", function(request, response) {
   var twilio = require('twilio');
-  var accountSID = 'AC17aa64762866918328ad2bdd4382f594';
-  var authToken = 'f91d9e5cdb3f08e2d17e410a4601841b';
-  var appSid = 'AP91929ec65ae9377af2c47cd245659c8a';
-  var capability = new twilio.Capability(accountSID, authToken);
+  var liveAccountSID = 'AC17aa64762866918328ad2bdd4382f594';
+  var testAccountSID = 'AC7316a035477a2f84679bdfbe42dd1546';
+  var liveAuthToken = 'f91d9e5cdb3f08e2d17e410a4601841b';
+  var testAuthToken = '194e2ad86f83ffa3890174f947ba805e';
+  var twilioAppSid = 'AP91929ec65ae9377af2c47cd245659c8a';
+  var capability = new twilio.Capability(liveAccountSID, liveAuthToken);
 
   //Create a capability token using the TwiML app with sid "AP123"
-  capability.allowClientOutgoing(appSid);
-  capability.allowClientOutgoing('tim');
-  capability.allowClientIncoming('joe');
-  capability.allowClientIncoming('tim');
+  capability.allowClientOutgoing(twilioAppSid);
+  // capability.allowClientIncoming('tim');
   var token = capability.generate();
   response.success(token);
 });
@@ -34,6 +30,7 @@ Parse.Cloud.define("welcome", function(request, response) {
   resp.say('Welcome to Twilio!');
 
   // Render the TwiML XML document
+  // response.header('Content-Type': 'text/xml');
   response.success(resp.toString());
 });
 
